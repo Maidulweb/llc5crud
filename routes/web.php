db93d11a00dfd6de6bb67aafbe656b1b5bf9a065
Maidul Islam <34934101+vatia60@users.noreply.github.com>
2020-02-19 19:33:39 +0600

    add code new hope

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,3 @@
 */
 
 Route::get('/', 'indexController@index')->name('index');
-
-Route::get('/register', 'LoginRegisterController@register')->name('register');
-Route::post('/register', 'LoginRegisterController@registerprocess');
-
-Route::get('/login', 'LoginRegisterController@login')->name('login');
-Route::post('/login', 'LoginRegisterController@loginprocess');
-
-
